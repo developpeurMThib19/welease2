f811f26a83fe4d00e2e8e4282ed57756f8d25f1e
Thibault MARTIN <developpeur.martin.thibault@gmail.com>
2022-08-20 07:15:29 +0200

    first commit

---
+++ weleasePagination/pages/index.tsx
@@ -0,0 +1,63 @@
+import type { NextPage } from 'next';
+import Head from 'next/head';
+import Pagination from '../components/Pagination/articles';
+import { usePagination, useUrlPagination } from 'url-pagination';
+import useSWR from 'swr';
+import { useEffect, useState } from "react"
+
+type Response = {
+  results: string[];
+  total: number;
+};
+
+
+
+const PaginationWithUrl = () => {
+  const [total, setTotal] = useState<number | undefined>()
+  const pagination = useUrlPagination({ perPage: 10, hotkeys: true, total });
+
+  const { data, error } = useSWR<Response>(
+    `/api/posts?offset=${pagination.offset}&limit=${pagination.perPage}`
+  );
+
+  useEffect(() => {
+    setTotal(data?.total)
+  }, [data?.total])
+
+  if (error) return <div>failed to load</div>;
+  if (!data) return <div>loading...</div>;
+
+  return (
+    <div>
+      <h2 className="text-2xl mb-4">Pagination that uses URL:</h2>
+      <ul className="mb-2">
+        {data.results.map((d, i) => (
+          <li key={i} className="p-1 list-disc">
+            {d}
+          </li>
+        ))}
+      </ul>
+      <Pagination total={data.total} {...pagination} />
+    </div>
+  );
+};
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <Head>
+        <title>url-pagination example</title>
+        <meta name="description" content="url-pagination" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className="flex justify-center mt-2">
+        <div className="flex flex-col gap-10">
+          <PaginationWithUrl />
+        </div>
+      </main>
+    </div>
+  );
+};
+
+export default Home;
